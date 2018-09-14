1ec6c24332a04335945dbc1710da47128c977abd
Maxim Vypov <mavy0313@netcracker.com>
2018-09-14 11:03:45 +0400

    tutorial part 4 is completed changed author

---
+++ src/com/gontuseries/hibernate/Main.java
@@ -0,0 +1,25 @@
+package com.gontuseries.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Student_Info student = new Student_Info();
+		student.setName("Gontu");
+		student.setRollNo(1);
+		
+		SessionFactory sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		
+		session.save(student);
+		
+		session.getTransaction().commit();
+		session.close();
+		sessionFactory.close();
+	}
+
+}
