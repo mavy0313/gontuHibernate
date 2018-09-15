efd53fbc7f9ae2e71dec304a4e8216706432aee2
Maxim Vypov <mavy0313@netcracker.com>
2018-09-15 10:26:25 +0400

    Part 10 Many To One Mapping

--- src/com/gontuseries/hibernate/Main.java
+++ src/com/gontuseries/hibernate/Main.java
@@ -9,20 +9,23 @@ import org.hibernate.cfg.AnnotationConfiguration;
 public class Main {
 
 	public static void main(String[] args) {
-		Student student = new Student();
-		student.setStudent_name("Gontu1");
+		StudentAddress studentAddress = new StudentAddress();
+		studentAddress.setAddress_detail("Hyderabad, India");
 		
-		StudentDetail studentDetail = new StudentDetail();
-		studentDetail.setStudent_mobile_number("99XXX60XXX");
+		Student student1 = new Student();
+		student1.setStudent_name("Gontu1");
+		student1.setStudentAddress(studentAddress);
 		
-		studentDetail.setStudent(student);	
-		student.setStudentDetail(studentDetail);
+		Student student2 = new Student();
+		student2.setStudent_name("Gontu2");
+		student2.setStudentAddress(studentAddress);			
 		
 		SessionFactory sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
 		Session session = sessionFactory.openSession();
 		session.beginTransaction();
 		
-		session.save(student);		
+		session.save(student1);
+		session.save(student2);	
 		
 		session.getTransaction().commit();
 		session.close();
--- src/com/gontuseries/hibernate/Student.java
+++ src/com/gontuseries/hibernate/Student.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -17,29 +18,32 @@ public class Student {
 	
 	private String student_name;
 	
-	@OneToOne(cascade = CascadeType.ALL)
-	@JoinColumn(name = "student_id")
-	private StudentDetail studentDetail;
-	
-	public StudentDetail getStudentDetail() {
-		return studentDetail;
+	@ManyToOne(cascade = CascadeType.ALL)
+	private StudentAddress studentAddress;	
+
+	public StudentAddress getStudentAddress() {
+		return studentAddress;
 	}
-	public void setStudentDetail(StudentDetail studentDetail) {
-		this.studentDetail = studentDetail;
+
+	public void setStudentAddress(StudentAddress studentAddress) {
+		this.studentAddress = studentAddress;
 	}
+
 	public int getStudent_id() {
 		return student_id;
 	}
+
 	public void setStudent_id(int student_id) {
 		this.student_id = student_id;
 	}
+
 	public String getStudent_name() {
 		return student_name;
 	}
+
 	public void setStudent_name(String student_name) {
 		this.student_name = student_name;
 	}
-	
-	
-	
+		
+
 }
