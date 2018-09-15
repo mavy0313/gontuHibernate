b3edd2c4f1059669cee6f23a589216034b45b322
Maxim Vypov <mavy0313@netcracker.com>
2018-09-15 18:56:49 +0400

    Part 17 Detached to Persistent state

--- src/com/gontuseries/hibernate/Main.java
+++ src/com/gontuseries/hibernate/Main.java
@@ -6,23 +6,36 @@ import org.hibernate.cfg.AnnotationConfiguration;
 
 public class Main {
 
-	public static void main(String[] args) {				
-		Student student = new Student();
-		student.setStudent_name("Gontu1");			
+	public static void main(String[] args) {
+//		Transient
+		Student student = null;
+//		student.setStudent_name("Gontu1");			
 		
 		SessionFactory sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
-		Session session = sessionFactory.openSession();
-		session.beginTransaction();				
+		Session session1 = sessionFactory.openSession();
+		session1.beginTransaction();				
 		
-		student.setStudent_name("Before Save");
+//		student.setStudent_name("Before Save");
+//		Persistent
+		student = (Student) session1.get(Student.class, 2);
+		System.out.println("Student name: " + student.getStudent_name());		
 		
-		session.save(student);
-		
-		student.setStudent_name("After Save");
-		
-		session.getTransaction().commit();
-		session.close();
+		session1.getTransaction().commit();
+		session1.close();
+//		Detached
 //		sessionFactory.close();
+		
+		student.setStudent_name("Modified in detached state");
+		
+		Session session2 = sessionFactory.openSession();
+		session2.beginTransaction();				
+		
+//		student.setStudent_name("Before Save");
+//		Persistent
+		session2.update(student);		
+		
+		session2.getTransaction().commit();
+		session2.close();
 	}
 
 }
